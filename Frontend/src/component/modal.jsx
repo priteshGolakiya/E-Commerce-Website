20387955de60de7f1105dc54acbab16898fb84f6
Pritesh Golakiya <priteshgolakiya1@gmail.com>
2024-05-24 15:56:53 +0530

    "Updated Modal component with animation and state management, and refactored AllProducts page to use the new Modal component and display product details in a table with a view details button."

--- Frontend/src/component/modal.jsx
+++ Frontend/src/component/modal.jsx
@@ -1,15 +1,31 @@
 /* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
 
 const Modal = ({ isOpen, onClose, children }) => {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    setIsAnimating(isOpen); // Enable animations when modal opens
+  }, [isOpen]);
+
+  const handleClose = () => {
+    setIsAnimating(false); // Disable animations when modal closes
+    setTimeout(() => onClose(), 300); // Close modal after animation completes
+  };
+
   return (
     <>
       {isOpen && (
-        <div className="fixed inset-0 z-50 overflow-auto bg-gray-900 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-8 rounded-lg max-w-md">
+        <div
+          className={`fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex justify-center items-center ${
+            isAnimating ? "fade-in" : "fade-out"
+          }`}
+        >
+          <div className="bg-white p-8 rounded-lg max-w-md transform transition-transform duration-300">
             <div className="flex justify-end">
               <button
                 className="text-gray-500 hover:text-gray-700 focus:outline-none"
-                onClick={onClose}
+                onClick={handleClose}
               >
                 <i className="fa-solid fa-x text-lg"></i>
               </button>
--- Frontend/src/pages/admin/adminPages/product/AllProducts.jsx
+++ Frontend/src/pages/admin/adminPages/product/AllProducts.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import summaryAPI from "../../../../utils/summaryAPI";
+import Modal from "../../../../component/modal"; // Import your Modal component
 
 const AllProducts = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [selectedProduct, setSelectedProduct] = useState(null); // State to track selected product for modal
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -40,63 +42,113 @@ const AllProducts = () => {
     );
   }
 
+  // Function to handle opening the modal
+  const handleOpenModal = (product) => {
+    setSelectedProduct(product);
+  };
+
+  // Function to handle closing the modal
+  const handleCloseModal = () => {
+    setSelectedProduct(null);
+  };
+
   return (
     <div className="max-w-6xl mx-auto p-5 bg-white shadow-lg rounded-lg">
       <h1 className="text-3xl font-bold mb-6 text-center">All Products</h1>
       {products.length === 0 ? (
         <div className="text-center text-lg">No products available</div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {products.map((product) => (
-            <div key={product._id} className="border p-4 rounded-lg shadow-md">
-              <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
-              <p className="mb-1">
-                <span className="font-medium">Brand:</span> {product.brand}
-              </p>
-              <p className="mb-1">
-                <span className="font-medium">Price:</span> ${product.price}
-              </p>
-              {product.discountPrice && (
-                <p className="mb-1 text-green-600">
-                  <span className="font-medium">Discount Price:</span> $
-                  {product.discountPrice}
-                </p>
-              )}
-              <p className="mb-1 text-blue-600">
-                <span className="font-medium">Final Price:</span> $
-                {product.finalPrice}
-              </p>
-              <p className="mb-1">
-                <span className="font-medium">Stock:</span> {product.stock}
-              </p>
-              {product.images && product.images.length > 0 && (
-                <div className="mb-2">
-                  {product.images.map((image, index) => (
-                    <img
-                      key={index}
-                      src={image}
-                      alt={`${product.name} ${index + 1}`}
-                      className="w-full h-48 object-cover mb-2 rounded-lg"
-                    />
-                  ))}
-                </div>
-              )}
-              <p className="mb-1">
-                <span className="font-medium">Category:</span>{" "}
-                {product.category.name}
-              </p>
-              <p className="mb-1">
-                <span className="font-medium">Subcategory:</span>{" "}
-                {product.subcategory.name}
-              </p>
-              <p className="mb-1">
-                <span className="font-medium">Delivery Options:</span>{" "}
-                {product.deliveryOptions}
-              </p>
-            </div>
-          ))}
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white border border-gray-300">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b">Name</th>
+                <th className="py-2 px-4 border-b">Brand</th>
+                <th className="py-2 px-4 border-b">Price</th>
+                <th className="py-2 px-4 border-b">Discount Price</th>
+                <th className="py-2 px-4 border-b">Final Price</th>
+                <th className="py-2 px-4 border-b">Stock</th>
+                <th className="py-2 px-4 border-b">Images</th>
+                <th className="py-2 px-4 border-b">Category</th>
+                <th className="py-2 px-4 border-b">Subcategory</th>
+                <th className="py-2 px-4 border-b">Delivery Options</th>
+                <th className="py-2 px-4 border-b">Options</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td className="py-3 px-4 border-b">{product.name}</td>
+                  <td className="py-3 px-4 border-b">{product.brand}</td>
+                  <td className="py-3 px-4 border-b">${product.price}</td>
+                  {product.discountPrice ? (
+                    <td className="py-3 px-4 border-b text-green-600">
+                      ${product.discountPrice}
+                    </td>
+                  ) : (
+                    <td className="py-3 px-4 border-b">N/A</td>
+                  )}
+                  <td className="py-3 px-4 border-b text-blue-600">
+                    ${product.finalPrice}
+                  </td>
+                  <td className="py-3 px-4 border-b">{product.stock}</td>
+                  <td className="py-3 px-4 border-b max-w-xs flex flex-col">
+                    {product.images && product.images.length > 0 && (
+                      <div className="">
+                        {product.images.map((image, index) => (
+                          <img
+                            key={index}
+                            src={image}
+                            alt={`${product.name} ${index + 1}`}
+                            className="w-12 h-12 object-cover rounded-lg cursor-pointer transform hover:scale-150 transition-transform"
+                          />
+                        ))}
+                      </div>
+                    )}
+                  </td>
+
+                  <td className="py-3 px-4 border-b">
+                    {product.category.name}
+                  </td>
+                  <td className="py-3 px-4 border-b">
+                    {product.subcategory.name}
+                  </td>
+                  <td className="py-3 px-4 border-b">
+                    {product.deliveryOptions}
+                  </td>
+                  <td>
+                    <button
+                      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+                      onClick={() => handleOpenModal(product)}
+                    >
+                      View Details
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       )}
+
+      {/* Render the Modal component conditionally */}
+      <Modal isOpen={selectedProduct !== null} onClose={handleCloseModal}>
+        {/* Modal content */}
+        {selectedProduct && (
+          <>
+            <h2 className="text-2xl font-semibold mb-4">
+              {selectedProduct.name}
+            </h2>
+            {/* Add more product details here */}
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+              onClick={handleCloseModal}
+            >
+              Close
+            </button>
+          </>
+        )}
+      </Modal>
     </div>
   );
 };
